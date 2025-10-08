75852abf398e9e9e00f28a40ab5ff5c09ba0c76e
Ledanis <daniel.puentes.n@gmail.com>
2025-10-08 05:40:10 -0300

    Se crea proyecto Inicial Base.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<script>
+</script>
+
+<main class="main">
+	<div class="pt-4">
+		<div class="video-panel"></div>
+	</div>
+	<div class="pt-4">
+		<div class="testing-card">
+			<p class="title text-center">!Bievenido a nuestra Plataforma!</p>
+		</div>
+	</div>
+</main>
+
+<style>
+	.main {
+		margin: 2vh;
+		height: 96vh;
+		display: flex;
+		gap: 2vh;
+	}
+
+	.video-panel {
+		background-color: #bca88d;
+		width: 40vw;
+		height: 98%;
+		margin-left: 2vh;
+		border-radius: 15px;
+		padding: 15px;
+	}
+	.testing-card {
+		background-color: #bca88d;
+		width: 54vw;
+		height: 98%;
+		margin-left: 2vh;
+		border-radius: 15px;
+		padding: 15px;
+	}
+
+	.title {
+		background-color: #7d8d86;
+		width: 100%;
+		font-family: 'Kode Mono';
+		font-size: 2rem;
+		border-radius: 7px;
+		color: #e9e8dd;
+	}
+</style>
